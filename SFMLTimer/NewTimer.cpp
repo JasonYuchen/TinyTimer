1f57a83f94fdb936680dccafc22db2e458117294
JasonCai <jasonyuchen@foxmail.com>
2018-01-15 20:56:06 +0800

    add window resize handler

--- SFMLTimer/NewTimer.cpp
+++ SFMLTimer/NewTimer.cpp
@@ -5,12 +5,16 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <vector>
+#include <string>
+#include <array>
 using namespace std;
 
 condition_variable cv;
 mutex mtx;
 sf::Time curTime;
-int eventFlag = 0;  //supurious wake
+int eventFlag = 0;                       //avoid supurious wake
+const size_t TimerInterval = 1000;       //timer frequency = 1/1000ms = 1Hz
+const size_t GUIResponseInterval = 50;   //GUI responses per 50ms
 
 bool eventPred()
 {
@@ -25,13 +29,12 @@ void Timer()
 	unique_lock<mutex> lock(mtx);
 	while (true)
 	{
-		if (cv.wait_for(lock, chrono::milliseconds(1000), eventPred) == false)   //over 1s
+		if (cv.wait_for(lock, chrono::milliseconds(TimerInterval), eventPred) == false)   //over 1s
 		{
 			auto t = clk.getElapsedTime();
 			clk.restart();
 			timer[flag] += t;
 			curTime = timer[flag];
-			cout << " over 1 s " << endl;
 		}
 		else                                                                     //event occurs
 		{
@@ -41,17 +44,56 @@ void Timer()
 			++flag;
 			flag %= 2;
 			curTime = timer[flag];
-			cout << " mouse event " << endl;
 			eventFlag = 0;
 		}
 	}
 }
 
+/* convert sf::Time to second, <1000 millisec = 0 sec */
+void sfTimeToSecond(const sf::Time &time, int &second)
+{
+	auto millisec = time.asMilliseconds();
+	second = millisec / 1000;
+}
+/* convert sf::Time to minute, <60 sec = 0 min */
+void sfTimeToMinute(const sf::Time &time, int &minute)
+{
+	int sec;
+	sfTimeToSecond(time, sec);
+	minute = sec / 60;
+}
+/* convert sf::Time to hour, <60 min = 0 hour */
+void sfTimeToHour(const sf::Time &time, int &hour)
+{
+	int min;
+	sfTimeToMinute(time, min);
+	hour = min / 60;
+}
+wstring manio(size_t val, size_t size, char fill = '0')
+{
+	auto ret = to_wstring(val);
+	wstring prefix(size - ret.size(), fill);
+	return prefix + ret;
+}
+/* normalize sf::time to hour, minute, second, millisecond of type string */
+array<wstring, 4> sfTimeToHMS(const sf::Time &time)
+{
+	int millisec, sec, min, hour;
+	millisec = time.asMilliseconds() % 1000;
+	sfTimeToSecond(time, sec);
+	sfTimeToMinute(time, min);
+	sfTimeToHour(time, hour);
+	return{ manio(hour,2), manio(min,2), manio(sec,2), manio(millisec,3) };
+}
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(720, 405), "TinyTimer");
-	sf::RectangleShape shape(sf::Vector2f(720, 405));
-	shape.setFillColor(sf::Color(102, 102, 102));
+	const size_t initFontSize = 55;
+	const size_t initFontPosX = 50, initFontPosY = 90;
+	const size_t initBackGroundSizeX = 720, initBackGroundSizeY = 405;
+
+	sf::RenderWindow window(sf::VideoMode(initBackGroundSizeX, initBackGroundSizeY), "TinyTimer");
+	sf::RectangleShape background(sf::Vector2f(initBackGroundSizeX, initBackGroundSizeY));
+	background.setFillColor(sf::Color(102, 102, 102));
 
 	sf::Font font;
 	if (!font.loadFromFile("C:/Windows/Fonts/simhei.ttf"))
@@ -60,31 +102,61 @@ int main()
 	}
 	sf::Text text;
 	text.setFont(font);
-	text.setCharacterSize(75);
+	text.setCharacterSize(initFontSize);
 	text.setFillColor(sf::Color::White);
-	text.setPosition(sf::Vector2f(160, 90));
+	text.setPosition(sf::Vector2f(initFontPosX, initFontPosY));
+
 	thread timerThread(Timer);
 	timerThread.detach();
+	
+	auto time = sfTimeToHMS(curTime);
+
 	while (window.isOpen())
 	{
 		sf::Event event;
-		text.setString(std::to_string(curTime.asSeconds()));
+		time = sfTimeToHMS(curTime);
+		text.setString(time[0] + L"Ð¡Ê±" + time[1] + L"·ÖÖÓ" + time[2] + L"Ãë" + time[3] + L"ºÁÃë");
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
+			{
 				window.close();
-			if (event.type == sf::Event::MouseButtonReleased)
+			}
+			else if (event.type == sf::Event::MouseButtonReleased)
 			{
 				eventFlag = 1;
 				cv.notify_one();
 			}
+			else if (event.type == sf::Event::Resized)
+			{
+				auto width = event.size.width;
+				auto height = event.size.height;
+				auto align = width;
+				auto factor = 1.0;
+				if (width * 9 > height * 16)   //display in 16:9
+				{
+					align = height;
+					factor = static_cast<double>(align) / initBackGroundSizeY;
+				}
+				else
+				{
+					align = width;
+					factor = static_cast<double>(align) / initBackGroundSizeX;
+				}
+				background.setScale(sf::Vector2f(initBackGroundSizeX * factor, initBackGroundSizeY * factor));
+				text.setCharacterSize(static_cast<unsigned int>(initFontSize * factor));
+				text.setPosition(sf::Vector2f(initFontPosX * factor, initFontPosY * factor));
+
+				sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+				window.setView(sf::View(visibleArea));
+			}
 		}
-		this_thread::sleep_for(chrono::milliseconds(50));
+		this_thread::sleep_for(chrono::milliseconds(GUIResponseInterval));
 		window.clear();
-		window.draw(shape);
+		window.draw(background);
 		window.draw(text);
 		window.display();
 	}
-
+	
 	return 0;
 }
